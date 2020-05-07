dde58236dcdd00a103ca83fe3c454d8857710297
Adam Such <asuch@tufted.home.adamsuch.io>
2020-05-07 13:58:48 +0100

    Adding IQ server URL to Jenkins Groovy script and Compose image

--- set-up-cloud.groovy
+++ set-up-cloud.groovy
@@ -4,6 +4,11 @@ import com.nirima.jenkins.plugins.docker.DockerTemplateBase
 import com.nirima.jenkins.plugins.docker.launcher.AttachedDockerComputerLauncher
 import io.jenkins.docker.connector.DockerComputerAttachConnector
 import io.jenkins.docker.connector.DockerComputerJNLPConnector;
+import hudson.EnvVars;
+import hudson.slaves.EnvironmentVariablesNodeProperty;
+import hudson.slaves.NodeProperty;
+import hudson.slaves.NodePropertyDescriptor;
+import hudson.util.DescribableList;
 import jenkins.model.Jenkins
 
 def dockerCloudParameters = [
@@ -47,6 +52,16 @@ def DockerTemplateParametersNPM = [
   remoteFs:       ''
 ]
 
+def dockerTemplateBaseParametersCompose = [
+  image:              'adamjwsuch/jenkins-node-compose:latest'
+]
+
+def DockerTemplateParametersCompose = [
+  instanceCapStr: '4',
+  labelString:    'compose-node',
+  remoteFs:       ''
+]
+
 DockerTemplateBase dockerTemplateBaseMaven = new DockerTemplateBase(
   dockerTemplateBaseParametersMaven.image
 )
@@ -83,12 +98,25 @@ DockerTemplate dockerTemplateNPM = new DockerTemplate(
   DockerTemplateParametersNPM.instanceCapStr
 )
 
+DockerTemplateBase dockerTemplateBaseCompose = new DockerTemplateBase(
+  dockerTemplateBaseParametersCompose.image
+)
+
+DockerTemplate dockerTemplateCompose = new DockerTemplate(
+  dockerTemplateBaseCompose,
+  new DockerComputerAttachConnector(),
+  DockerTemplateParametersCompose.labelString,
+  DockerTemplateParametersCompose.remoteFs,
+  DockerTemplateParametersCompose.instanceCapStr
+)
+
 DockerCloud dockerCloud = new DockerCloud(
   dockerCloudParameters.name,
   [
     dockerTemplateMaven,
     dockerTemplateGradle,
-    dockerTemplateNPM 
+    dockerTemplateNPM,
+    dockerTemplateCompose 
   ],
   dockerCloudParameters.serverUrl,
   dockerCloudParameters.containerCapStr,
@@ -113,9 +141,36 @@ dockerTemplateGradle.connector.setUser("jenkins")
 dockerTemplateNPM.setMode(Node.Mode.EXCLUSIVE)
 dockerTemplateNPM.setRemoteFs("/home/jenkins")
 dockerTemplateNPM.connector.setUser("jenkins")
+dockerTemplateCompose.setMode(Node.Mode.EXCLUSIVE)
+dockerTemplateCompose.setRemoteFs("/home/jenkins")
+dockerTemplateCompose.connector.setUser("jenkins")
 
-clouds*.name.each { cloudName ->
-     println "Configured docker cloud ${cloudName}"
+println "Configured docker cloud"
+
+//Create global environmental variable for IQ URL
+public createGlobalEnvironmentVariables(String key, String value){
+
+        Jenkins instance = Jenkins.getInstance();
+
+        DescribableList<NodeProperty<?>, NodePropertyDescriptor> globalNodeProperties = instance.getGlobalNodeProperties();
+        List<EnvironmentVariablesNodeProperty> envVarsNodePropertyList = globalNodeProperties.getAll(EnvironmentVariablesNodeProperty.class);
+
+        EnvironmentVariablesNodeProperty newEnvVarsNodeProperty = null;
+        EnvVars envVars = null;
+
+        if ( envVarsNodePropertyList == null || envVarsNodePropertyList.size() == 0 ) {
+            newEnvVarsNodeProperty = new hudson.slaves.EnvironmentVariablesNodeProperty();
+            globalNodeProperties.add(newEnvVarsNodeProperty);
+            envVars = newEnvVarsNodeProperty.getEnvVars();
+        } else {
+            envVars = envVarsNodePropertyList.get(0).getEnvVars();
+        }
+        envVars.put(key, value)
+        instance.save()
 }
+// *** Please edit the IQ URL to match you environment ***
+createGlobalEnvironmentVariables('IQserver','http://iq:8080')
+println "Configured Environmental Variable for IQ server URL, please chack it matches your configuration"
+
 // save current Jenkins state to disk
 jenkins.save()
